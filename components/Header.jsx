fc08c2bd69a01ca053255e9ee4e0036282eb26a1
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-08 12:48:30 +0200

    function fakeLogOut

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,3 +12,37 @@ export default function Header() {
     function fakeLogOut() {
         localStorage.removeItem("loggedin")
     }
+
+    return (
+        <header>
+            <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+                <NavLink
+                    to="/host"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="/vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+                <Link to="login" className="login-link">
+                    <img
+                        src={imageUrl}
+                        className="login-icon"
+                    />
+                </Link>
+                <button onClick={fakeLogOut}>X</button>
+            </nav>
+        </header>
+    )
+}
