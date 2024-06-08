9622f0ae177fdb1f2c62394ffce6d7267a0f596a
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-08 12:50:03 +0200

    navigation

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -7,3 +7,41 @@ export default function HostLayout() {
         textDecoration: "underline",
         color: "#161616"
     }
+
+    return (
+        <>
+            <nav className="host-nav">
+                <NavLink
+                    to="."
+                    end
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Dashboard
+                </NavLink>
+
+                <NavLink
+                    to="income"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Income
+                </NavLink>
+                
+                <NavLink
+                    to="vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+
+                <NavLink
+                    to="reviews"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Reviews
+                </NavLink>
+
+            </nav>
+            <Outlet />
+        </>
+    )
+}
