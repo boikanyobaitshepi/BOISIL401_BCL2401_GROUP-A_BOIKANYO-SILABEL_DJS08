e47a08e4134c652860f2884440d2fb84ceddf24e
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-08 12:49:02 +0200

    export default function HostLayout

---
+++ components/HostLayout.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { NavLink, Outlet } from "react-router-dom"
+
+export default function HostLayout() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
