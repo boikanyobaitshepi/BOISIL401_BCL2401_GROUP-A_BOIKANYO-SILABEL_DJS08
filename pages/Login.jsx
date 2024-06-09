cb035792d727da180d6547a539a3622b41d96e9b
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-09 18:35:57 +0200

    log in function

---
+++ pages/Login.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+import { loginUser } from "../api"
+
+export default function Login() {
+    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
+
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const from = location.state?.from || "/host";
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setStatus("submitting")
+        loginUser(loginFormData)
+            .then(data => {
+                setError(null)
+                localStorage.setItem("loggedin", true)
+                navigate(from, { replace: true })
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            })
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setLoginFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+   
